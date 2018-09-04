ecbab8e8bd775cb5451c66074160d67318e39655
ish1r0k1 <dev.ish1r0k1@gmail.com>
2018-09-04 22:51:22 +0900

    feat: サンプルアプリの実装

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('Hello.')
+import * as dotenv from "dotenv";
+import * as Botkit from "botkit";
+
+dotenv.config()
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+const controler = Botkit.slackbot({
+  debug: false
+})
+
+const bot = controler.spawn({
+  token: process.env.token
+}).startRTM()
+
+controler.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', (bot, message) => {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face'
+  }, (err, res) => {
+    if (err) {
+      bot.botkit.log('Failed to add emoji reaction :(', err);
+    }
+  });
+
+  controler.storage.users.get(message.user, (err, user) => {
+    bot.botkit.log('user', user)
+
+    if (user && user.name) {
+      bot.reply(message, 'Hello ' + user.name + '!!');
+    } else {
+      bot.reply(message, 'Hello.');
+    }
+  })
+})
